955f81948eaa2f8c773cb3ff54b627c0c45ac51d
zzboncak <zachary.zboncak@gmail.com>
2021-09-06 14:16:46 -0500

    put in basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,18 +7,29 @@ function App(): JSX.Element {
     <div className="App">
       <nav className="nav-bar">
         <header className="App-header">
-          <Link to="/about">About Us</Link>
+          <Link to="/">About Us</Link>
           <Link to="/registry">Registry</Link>
           <Link to="/rsvp">RSVP</Link>
           <Link to="/info">Event Info</Link>
         </header>
       </nav>
-      <main>
-        <h1>We&#39;re getting married</h1>
-        <sub>Hel(l yeah)geson</sub>
-
-        <button>RSVP bish</button>
-      </main>
+      <Switch>
+        <Route path="/registry">
+          <section>I am the registry</section>
+        </Route>
+        <Route path="/rsvp">
+          <button>RSVP bish</button>
+        </Route>
+        <Route path="/info">
+          <section>I am the info</section>
+        </Route>
+        <Route exact path="/">
+          <main>
+            <h1>We&#39;re getting married</h1>
+            <sub>Hel(l yeah)geson</sub>
+          </main>
+        </Route>
+      </Switch>
     </div>
   );
 }
