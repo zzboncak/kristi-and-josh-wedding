00ab5ec0a125567349d8b1a555787e7ead0ad8d6
zzboncak <zachary.zboncak@gmail.com>
2021-09-14 11:38:07 -0500

    added to types and utilities

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,3 +1,6 @@
+import { API_ENDPOINT } from "./config";
+import { Invite, InviteUpdateRequest, Person } from "./types";
+
 export function api<T>(
   url: string,
   init?: RequestInit | undefined
@@ -8,4 +11,47 @@ export function api<T>(
     }
     return response.json();
   });
+}
+
+export function fetchInviteAndPeople(
+  keyword: string
+): Promise<[Invite, Person[]]> {
+  return Promise.all([
+    fetch(`${API_ENDPOINT}/invites/${keyword.toLowerCase()}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then((data: Invite[]) => data[0]),
+    fetch(`${API_ENDPOINT}/people/${keyword.toLowerCase()}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then((data: Person[]) => data)
+  ]);
+}
+
+export function updateInvite(
+  inviteId: number,
+  fieldsToUpdate: Partial<InviteUpdateRequest>
+): Promise<Invite> {
+  return fetch(`${API_ENDPOINT}/invites/update/${inviteId}`, {
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(fieldsToUpdate)
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    })
+    .then((invite: Invite) => invite);
 }
