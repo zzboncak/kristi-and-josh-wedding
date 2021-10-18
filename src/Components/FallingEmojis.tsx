b46b9c1daca2cc253aa4f4f6044754d8bfdabcac
zzboncak <zachary.zboncak@gmail.com>
2021-10-18 09:39:04 -0500

    more tweaks to falling emoji

--- src/Components/FallingEmojis.tsx
+++ src/Components/FallingEmojis.tsx
@@ -1,17 +1,6 @@
 import React from "react";
 import "./FallingEmojis.css";
 
-const styles = {
-  FallingEmojis: "emoji-container",
-  emoji: "emoji",
-  emojiAnimation0: "animation1",
-  float: "squiggle_fall",
-  emojiAnimation1: "animation2",
-  emojiAnimation2: "animation3",
-  emojiAnimation3: "animation4",
-  emojiAnimation4: "animation5"
-};
-
 export const FallingEmojis: React.FC<{ emoji: string }> = ({
   emoji
 }) => {
@@ -20,11 +9,11 @@ export const FallingEmojis: React.FC<{ emoji: string }> = ({
     () => Math.random() - 0.5
   );
   const animations = [
-    styles.emojiAnimation0,
-    styles.emojiAnimation1,
-    styles.emojiAnimation2,
-    styles.emojiAnimation3,
-    styles.emojiAnimation4
+    "animation1",
+    "animation2",
+    "animation3",
+    "animation4",
+    "animation5"
   ];
   return (
     <div className="emoji-container">
