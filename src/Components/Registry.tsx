2679731664d060939680f3cd1e5aa7b7dd3620c5
zzboncak <zachary.zboncak@gmail.com>
2021-10-21 16:12:11 -0500

    made flip card its own component for reuse

--- src/Components/Registry.tsx
+++ src/Components/Registry.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FlipCard } from "./FlipCard";
 import "./Registry.css";
 
 export const Registry: React.FC = () => {
@@ -7,36 +8,16 @@ export const Registry: React.FC = () => {
       <h2 className="header">
         Help us fill our <i className="fas fa-home"></i>
       </h2>
-      <div className="card-container">
-        <div className="registry-item">
-          <div className="front">
-            <span>Target</span>
-          </div>
-          <a
-            href="https://www.target.com/gift-registry/gift-giver?registryId=a7f8b4f0-0d28-11ec-bb7c-df277178efe4&type=WEDDING"
-            target="_blank"
-            rel="noreferrer"
-            className="back"
-          >
-            Lots of basics
-          </a>
-        </div>
-      </div>
-      <div className="card-container">
-        <div className="registry-item">
-          <div className="front">
-            <span>Zola</span>
-          </div>
-          <a
-            href="http://www.zola.com/registry/kristiandjoshua2022"
-            target="_blank"
-            rel="noreferrer"
-            className="back"
-          >
-            Chip in on a group gift
-          </a>
-        </div>
-      </div>
+      <FlipCard
+        front="Target"
+        back="Lots of basics"
+        link="https://www.target.com/gift-registry/gift-giver?registryId=a7f8b4f0-0d28-11ec-bb7c-df277178efe4&type=WEDDING"
+      />
+      <FlipCard
+        front="Zola"
+        back="Chip in on a group gift"
+        link="http://www.zola.com/registry/kristiandjoshua2022"
+      />
     </section>
   );
 };
